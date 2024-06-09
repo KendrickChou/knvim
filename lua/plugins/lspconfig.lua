cf6948dd08b04ce96e5c75ef7f792e3221d0d530
KendrickChou <847361724@qq.com>
2024-06-09 21:41:14 +0800

    add formatters

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -191,6 +191,10 @@ return {
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				'stylua', -- Used to format Lua code
+				'black',
+				'isort',
+				'clang-format',
+				'shfmt',
 			})
 			require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
