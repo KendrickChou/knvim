0b5f2447cbc82bb850af993f35b31d7029c70749
KendrickChou <847361724@qq.com>
2024-06-14 09:53:34 +0800

    minor

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,14 +5,16 @@ return {
 		config = function()
 			vim.g.vimtex_mappings_prefix = ','
 			vim.g.vimtex_view_method = 'skim'
-			vim.g.vimtex_compiler_latexmk_engines = {
-				_ = '-xelatex',
-			}
+			-- vim.g.vimtex_compiler_latexmk_engines = {
+			-- 	_ = '-xelatex',
+			-- }
 			vim.g.vimtex_view_skim_sync = 1
 			-- vim.g.vimtex_view_skim_activate = 1
 			vim.g.tex_comment_nospell = 1
 			vim.g.vimtex_quickfix_open_on_warning = 0
 			vim.g.vimtex_complete_enabled = 0
+			-- vim.g.vimtex_lint_chktex_ignore_warnings = 0
+			-- vim.g.vimtex_log_ignore = { 'thebibliography' }
 			-- vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
 		end,
 	},
