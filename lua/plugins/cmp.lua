5c033a83b573c2e4a8370c430ed7fa6c875e38b6
KendrickChou <847361724@qq.com>
2024-10-11 09:36:33 +0800

    modify cmp logic

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -97,6 +97,7 @@ return {
 					},
 				},
 
+
 				-- documentation = {
 				-- 	border = border 'CmpDocBorder',
 				-- },
@@ -121,6 +122,13 @@ return {
 					--  This will auto-import if your LSP supports it.
 					--  This will expand snippets if the LSP sent a snippet.
 					-- ['<C-y>'] = cmp.mapping.confirm { select = true },
+					['<Enter>'] = cmp.mapping(function(fallback)
+						if cmp.visible() and has_words_before() then
+							cmp.confirm { select = true }
+						else
+							fallback()
+						end
+					end, { 'i', 's' }),
 
 					['<Tab>'] = cmp.mapping(function(fallback)
 						if cmp.visible() and has_words_before() then
@@ -131,6 +139,7 @@ return {
 							fallback()
 						end
 					end, { 'i', 's' }),
+
 					['<S-Tab>'] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_prev_item()
